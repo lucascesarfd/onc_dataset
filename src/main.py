1080f367d10f7d5e21b799d360cfbfd0ae4b054c
Lucas Domingos <lucascesarfd@outlook.com>
2022-02-17 19:07:28 -0300

    Rename get_dataset.py to main.py and update with the new stages

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import argparse
+import os
 
-from utils import bcolors, create_dir
+from utils import bcolors, create_dir, get_exclusion_radius
 from download import query_onc_deployments, download_files
 from parse import parse_ais_to_json
-from clean import clean_ais_data
+from clean import clean_ais_data, clean_ctd_data
 from combine import combine_deployment_ais_data
 from identify import identify_scenarios
 from format import group_wav_from_range
+from generate_metadata import generate_full_metadata, generate_balanced_metadata, get_metadata_for_small_times, split_dataset
 
 
 def create_parser():
@@ -14,14 +16,6 @@ def create_parser():
     parser = argparse.ArgumentParser(description="Dataset Preparation Script")
 
     # Add the arguments
-    parser.add_argument(
-        "--work_dir",
-        "-w",
-        type=str,
-        default="/workspaces/underwater/dataset",
-        help="The path to the workspace directory.",
-    )
-
     parser.add_argument(
         "onc_token",
         type=str,
@@ -31,12 +25,20 @@ def create_parser():
     )
 
     parser.add_argument(
+        "--work_dir",
+        "-w",
+        type=str,
+        default="/workspaces/underwater/dataset",
+        help="The path to the workspace directory.",
+    )
+
+    parser.add_argument(
         "--steps",
         "-s",
         action='store',
         type=int,
         nargs="+",
-        default=[0, 1, 2, 3, 4, 5, 6, 7],
+        default=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         help="The numbers related to the steps that you want to execute. "
         "By default, all tests are executed."
         "0 - Query ONC deployments; "
@@ -46,7 +48,45 @@ def create_parser():
         "4 - Clean AIS data; "
         "5 - Combine deployment AIS data; "
         "6 - Identify scenarios; "
-        "7 - Classify WAV files from range.",
+        "7 - Classify WAV files from range; "
+        "8 - Download CTD files; "
+        "9 - Clean CTD files; "
+        "10 - Generate the metadata for the full dataset; "
+        "11 - Generate a balanced version of the full dataset; "
+        "12 - Generate metadata for small periods of duration; "
+        "13 - Split dataset into Train, Test and Validation.",
+    )
+
+    parser.add_argument(
+        "--max_inclusion_radius",
+        "-m",
+        type=float,
+        default=15000.0,
+        help="The maximum distance (metres) that a vessel can be from the hydrophone. Used for the whole dataset.",
+    )
+    
+    parser.add_argument(
+        "--inclusion_radius",
+        "-i",
+        type=int,
+        default=1000,
+        help="The maximum distance (metres) that a vessel can be from the hydrophone. Used on the subset",
+    )
+
+    parser.add_argument(
+        "--seconds",
+        "-t",
+        type=int,
+        default=10,
+        help="The size of each audio sample in metadata.",
+    )
+
+    parser.add_argument(
+        "--metadata_file",
+        "-f",
+        type=str,
+        default="metadata",
+        help="The name of the metadata file without the extension. Assumed to be .csv",
     )
 
     return parser
@@ -70,12 +110,23 @@ def _main():
     scenario_intervals_directory = create_dir(working_directory, "06a_scenario_intervals")
     interval_ais_data_directory = create_dir(working_directory, "06b_interval_ais_data")
     classified_wav_directory = create_dir(working_directory, "07_classified_wav_files")
+    raw_ctd_directory = create_dir(working_directory, "08_raw_ctd_files")
+    clean_ctd_directory = create_dir(working_directory, "09_cleaned_ctd_files")
 
     #token = "155f45d8-30ec-4e7f-a866-167c7d424635"
     token = args.onc_token
 
     # The maximum distance (metres) that a vessel can be from the hydrophone before we start caring about it.
-    inclusion_radius = 15000.0
+    max_inclusion_radius = args.max_inclusion_radius
+    inclusion_radius = args.inclusion_radius
+
+    # The size of each audio sample in metadata.
+    seconds = args.seconds
+
+    # The matadata file name.
+    metadata_file = args.metadata_file
+
+    root_path = os.path.join(classified_wav_directory, f"inclusion_{inclusion_radius}_exclusion_{get_exclusion_radius(inclusion_radius)}")
 
     if 0 in args.steps:
         print(f"\n{bcolors.HEADER}Querying Ocean Natworks Canada for Deployments{bcolors.ENDC}")
@@ -116,7 +167,7 @@ def _main():
             deployment_directory,
             parsed_ais_directory,
             clean_ais_directory,
-            _inclusion_radius=inclusion_radius,
+            _inclusion_radius=max_inclusion_radius,
             use_all_threads=False,
         )
 
@@ -129,7 +180,7 @@ def _main():
             clean_ais_directory,
             combined_deployment_directory,
             run_shortest,
-            inclusion_radius,
+            max_inclusion_radius,
             use_all_threads=False,
         )
 
@@ -144,12 +195,64 @@ def _main():
         )
 
     if 7 in args.steps:
-        print(f"\n{bcolors.HEADER}Classifying dataset into the choosed range{bcolors.ENDC}")
+        print(f"\n{bcolors.HEADER}Classifying the dataset into the chosen range{bcolors.ENDC}")
         group_wav_from_range(
             classified_wav_directory,
             scenario_intervals_directory,
+            interval_ais_data_directory,
             raw_wav_directory,
-            2000,
+            inclusion_radius,
+        )
+
+    if 8 in args.steps:
+        print(f"\n{bcolors.HEADER}Downloading Conductivity Temperature Depth Files{bcolors.ENDC}")
+        download_files(
+            raw_ctd_directory,
+            deployment_directory,
+            token,
+            file_type="CTD",
+        )
+
+    if 9 in args.steps:
+        print(f"\n{bcolors.HEADER}Cleaning CTD data{bcolors.ENDC}")
+        clean_ctd_data(
+            deployment_directory,
+            raw_ctd_directory,
+            clean_ctd_directory,
+            use_all_threads=False,
+        )
+
+    if 10 in args.steps:
+        print(f"\n{bcolors.HEADER}Generating the metadata for the full dataset{bcolors.ENDC}")
+        generate_full_metadata(
+            root_path,
+            clean_ctd_directory,
+            interval_ais_data_directory,
+            inclusion_radius,
+        )
+
+    if 11 in args.steps:
+        print(f"\n{bcolors.HEADER}Generating the balanced metadata version{bcolors.ENDC}")
+        generate_balanced_metadata(
+            f"{metadata_file}.csv",
+            root_path,
+        )
+
+    if 12 in args.steps:
+        print(f"\n{bcolors.HEADER}Splitting dataset into small periods of time{bcolors.ENDC}")
+        get_metadata_for_small_times(
+            root_path,
+            f"{metadata_file}.csv",
+            seconds,
+        )
+
+    if 13 in args.steps:
+        print(f"\n{bcolors.HEADER}Splitting dataset into train, test and validation datasets{bcolors.ENDC}")
+        split_dataset(
+            root_path,
+            f"{metadata_file}_{seconds}s.csv",
+            validation_split=0.2,
+            test_split=0.1
         )
 
 
